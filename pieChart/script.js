97aabbe094e511d154a82e886d6f28376af04860
Nicholas Gentile <8gentile@gmail.com>
2017-12-04 23:15:40 -0500

    animate annulus

--- pieChart/script.js
+++ pieChart/script.js
@@ -11,20 +11,34 @@ var svgContainer = d3
 
 var data = [1, 2, 1, 5, 6, 8, 10];
 
-var arc = d3
-  .arc()
-  .outerRadius(200)
-  .innerRadius(100);
+function render() {
+  d3.select("g").remove();
 
-var group = svgContainer
-  .append("g")
-  .attr("transform", "translate(" + 375 + "," + 300 + ")");
+  var arc = d3
+    .arc()
+    .outerRadius(200)
+    .innerRadius(100);
 
-var arcs = d3.pie()(data);
+  var group = svgContainer
+    .append("g")
+    .attr("transform", "translate(" + 375 + "," + 300 + ")");
 
-arcs.forEach(function(d, i) {
-  group
-    .append("path")
-    .attr("d", arc(d))
-    .attr("fill", color(i));
-});
+  var arcs = d3.pie()(data);
+
+  arcs.forEach(function(d, i) {
+    group
+      .append("path")
+      .attr("fill", color(i))
+      .transition()
+      .duration(2000)
+      .attrTween("d", function() {
+        var start = { startAngle: 0, endAngle: 0 };
+        var interpolate = d3.interpolate(start, d);
+        return function(t) {
+          return arc(interpolate(t));
+        };
+      });
+  });
+}
+
+setInterval(render, 3000);
