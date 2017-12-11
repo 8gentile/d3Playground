715391b201a84999e3003ee7a9378413c975ac1c
Nicholas Gentile <endureliberty@yahoo.com>
2017-12-11 17:36:38 -0500

    Properly bind data

--- pieChart/src/script.js
+++ pieChart/src/script.js
@@ -21,48 +21,32 @@ function render() {
     .outerRadius(outerRadius)
     .innerRadius(innerRadius);
 
-  var group = svgContainer
+  var annulusG = svgContainer
     .append("g")
     .attr("transform", "translate(" + 375 + "," + 300 + ")");
 
   var arcs = d3.pie()(data);
 
-  arcs.forEach(function(d, i) {
-    let newArc = group.append("path")
-      .attr("fill", color(i))
-      .attr("cursor", "pointer")
-
-      newArc.transition()
-      .duration(2000)
-      .attrTween("d", function() {
-        var start = { startAngle: 0, endAngle: 0 };
-        var interpolate = d3.interpolate(start, d);
-        return function(t) {
-          return arc(interpolate(t));
-        };
-      });
-
-      newArc
-      .on('mouseover', function(d) {
-        debugger
-        d3.select(this)
-          .transition()
-          .delay(200)
-          .attrTween("d", function(d) {
-            debugger
-            let i = d3.interpolate(d.outerRadius, outerRadius);
-            return function(t) { 
-              d.outerRadius = i(t); 
-              return arc(d); 
-            };
-          });
-      });
-      // .on('mouseout', arcTween(outerRadius - 20, 150));
-  });
+  annulusG
+    .selectAll("path")
+    .data(arcs)
+    .enter()
+    .append("path")
+    .attr("fill", (d, i) => color(i))
+    .attr("cursor", "pointer")
+    .transition()
+    .duration(2000)
+    .attrTween("d", function(d, i) {
+      var start = { startAngle: 0, endAngle: 0 };
+      var interpolate = d3.interpolate(start, d);
+      return function(t) {
+        return arc(interpolate(t));
+      };
+    });
 }
 
 function arcTween(outerRadius, delay) {
-  return 
+  return;
 }
 
 render();
